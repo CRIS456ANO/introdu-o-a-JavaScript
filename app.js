5e414fabdb796bc8964363c79ae166aa95aa352e
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2022-09-06 17:11:09 -0300

    tentativa de construção de um programa

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-var nome = window.prompt("Como você se chama?")
-window.alert(`Muito prazer ${nome}. Bem vindo.`)
+// var nome = window.prompt("Como você se chama?")
+// window.alert(`Muito prazer ${nome}. Bem vindo.`)
+function carregar() {
+    var msgHora = window.document.getElementById('msg')
+    var imagem = window.document.getElementById('fotodahora')
+    var now = new Date()
+    var hora = now.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora > 0 && hora < 12) {
+        imagem.src = '../imagens/foto-manha.png'
+    } else if (hora < 18) {
+        imagem.src = '../imagens/fototarde.png'
+    } else {
+        imagem.src = '../imagens/fotonoite.png'
+    }
+}
