41437e12f95034c8da0ac60422fdd2e2eb27cd4d
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2022-09-06 15:30:30 -0300

    organizando os programas

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-const switcher = document.querySelector('btn');
-switcher.addEventListener('click',function(){
-    document.body.classList.toggle('dark-theme')
-
-    var className = document.body.className;
-    if (className == "light-theme") {
-        this.textContent = "Dark";
-    }
-    else {
-        this.textContent = "light";
-    }
-});
+var nome = window.prompt("Como você se chama?")
+window.alert(`Muito prazer ${nome}. Bem vindo.`)
