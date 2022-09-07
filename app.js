153ea7b468223cd8f91485ae3686e0317e46a25b
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2022-09-07 10:58:25 -0300

    programa "hora do dia" funciona.

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-// var nome = window.prompt("Como você se chama?")
-// window.alert(`Muito prazer ${nome}. Bem vindo.`)
+function saudar() {
+    var nome = window.prompt("Como você se chama?")
+    window.alert(`Muito prazer ${nome}. Bem vindo.`)
+}
+
+
 function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var now = new Date()
     var hora = now.getHours()
     var minutos = now.getMinutes()
-    msg.innerHTML = `Agora são ${hora}:${minutos} horas.`
-
+    
     if (hora >= 0 && hora < 12) {
-        img.src = '../imagens/foto-manha.png'
-        document.body.style.background = '#ffffff'
+        img.src = '../imagens/fotomanha.png'
+        document.body.style.background = '#d99b63'
+        msg.innerHTML = `Bom dia, agora são ${hora}:${minutos} horas.`
     } else if (hora < 18) {
         img.src = '../imagens/fototarde.png'
+        document.body.style.background = '#ca692a'
+        msg.innerHTML = `Boa tarde, agora são ${hora}:${minutos} horas.`
     } else {
         img.src = '../imagens/fotonoite.png'
+        document.body.style.background = '#1a1f29'
+        msg.innerHTML = `Boa noite, agora são ${hora}:${minutos} horas.`
     }
 }
