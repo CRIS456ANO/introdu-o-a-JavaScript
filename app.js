f7e29931f993fead4035e94c7a425ce22215e17e
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2022-09-07 14:59:13 -0300

    atualização

--- app.js
+++ app.js
@@ -12,16 +12,28 @@ function carregar() {
     var minutos = now.getMinutes()
     
     if (hora >= 0 && hora < 12) {
-        img.src = '../imagens/fotomanha.png'
+        img.src = '../../imagens/fotomanha.png'
         document.body.style.background = '#d99b63'
         msg.innerHTML = `Bom dia, agora são ${hora}:${minutos} horas.`
     } else if (hora < 18) {
-        img.src = '../imagens/fototarde.png'
+        img.src = '../../imagens/fototarde.png'
         document.body.style.background = '#ca692a'
         msg.innerHTML = `Boa tarde, agora são ${hora}:${minutos} horas.`
     } else {
-        img.src = '../imagens/fotonoite.png'
+        img.src = '../../imagens/fotonoite.png'
         document.body.style.background = '#1a1f29'
         msg.innerHTML = `Boa noite, agora são ${hora}:${minutos} horas.`
     }
 }
+
+function calcular() {
+    var txtv = window.document.querySelector('input#txtvel')
+    var res = window.document.querySelector('div#res')
+    var velocidade = Number(txtv.value)
+    res.innerHTML = `<p>A sua velocidade atual é de <strong>${velocidade} KM/h</strong></p>`
+    if (velocidade > 65) {
+res.innerHTML += `<p>Ela está acima de 60 Km/h e por isso você foi <strong>MULTADO</strong>. Procure pagá-la o quanto antes.</p>`
+    }
+    res.innerHTML += `Tome sempre cuidado, use sempre cinto de segurança.`
+     
+ }
